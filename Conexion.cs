d0d515d6a12bb11fb1c0324abc1ff3d03b9f3f1d
ArturCA177 <106893756+ArturCA177@users.noreply.github.com>
2022-12-20 19:35:47 -0600

    Add files via upload

---
+++ Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ProyectoBDD
+{
+    class Conexion
+    {
+        public static SqlConnection conectaSQL()
+        {
+            SqlConnection cxn = new SqlConnection();
+            cxn.ConnectionString =
+              "Data Source=MSI;" +
+              "Initial Catalog=AdventureWorks2019;" +
+              "User id=sa;" +
+              "Password=@Drackmaster_12;";
+            return cxn;
+        }
+
+        public void desconectaSQL(SqlConnection cxn)
+        {
+            cxn.Close();
+        }
+    }
+}
