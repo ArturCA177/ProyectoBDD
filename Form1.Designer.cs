02373972d0e51bab6fc664b9fb067c7ba4198467
Bryanxdddd <107012980+Bryanxdddd@users.noreply.github.com>
2022-12-20 20:59:07 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace ProyectoBDD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblConsultas = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtp2 = new System.Windows.Forms.DateTimePicker();
+            this.dtp1 = new System.Windows.Forms.DateTimePicker();
+            this.btnP = new System.Windows.Forms.Button();
+            this.txtP3 = new System.Windows.Forms.TextBox();
+            this.txtP2 = new System.Windows.Forms.TextBox();
+            this.lblP3 = new System.Windows.Forms.Label();
+            this.lblP2 = new System.Windows.Forms.Label();
+            this.txtP1 = new System.Windows.Forms.TextBox();
+            this.lblP1 = new System.Windows.Forms.Label();
+            this.lblCV = new System.Windows.Forms.Label();
+            this.btnC1 = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            this.btnC3 = new System.Windows.Forms.Button();
+            this.btnC4 = new System.Windows.Forms.Button();
+            this.btnC5 = new System.Windows.Forms.Button();
+            this.btnC6 = new System.Windows.Forms.Button();
+            this.btnC7 = new System.Windows.Forms.Button();
+            this.btnC8 = new System.Windows.Forms.Button();
+            this.btnC9 = new System.Windows.Forms.Button();
+            this.btnC10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 451);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1096, 200);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // lblConsultas
+            // 
+            this.lblConsultas.AutoSize = true;
+            this.lblConsultas.Location = new System.Drawing.Point(24, 20);
+            this.lblConsultas.Name = "lblConsultas";
+            this.lblConsultas.Size = new System.Drawing.Size(75, 20);
+            this.lblConsultas.TabIndex = 2;
+            this.lblConsultas.Text = "Consultas:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dtp2);
+            this.panel1.Controls.Add(this.dtp1);
+            this.panel1.Controls.Add(this.btnP);
+            this.panel1.Controls.Add(this.txtP3);
+            this.panel1.Controls.Add(this.txtP2);
+            this.panel1.Controls.Add(this.lblP3);
+            this.panel1.Controls.Add(this.lblP2);
+            this.panel1.Controls.Add(this.txtP1);
+            this.panel1.Controls.Add(this.lblP1);
+            this.panel1.Location = new System.Drawing.Point(16, 255);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 167);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // dtp2
+            // 
+            this.dtp2.Location = new System.Drawing.Point(310, 96);
+            this.dtp2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtp2.Name = "dtp2";
+            this.dtp2.Size = new System.Drawing.Size(228, 27);
+            this.dtp2.TabIndex = 8;
+            this.dtp2.Visible = false;
+            // 
+            // dtp1
+            // 
+            this.dtp1.Location = new System.Drawing.Point(24, 96);
+            this.dtp1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtp1.Name = "dtp1";
+            this.dtp1.Size = new System.Drawing.Size(228, 27);
+            this.dtp1.TabIndex = 7;
+            this.dtp1.Visible = false;
+            // 
+            // btnP
+            // 
+            this.btnP.Location = new System.Drawing.Point(927, 71);
+            this.btnP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnP.Name = "btnP";
+            this.btnP.Size = new System.Drawing.Size(86, 31);
+            this.btnP.TabIndex = 6;
+            this.btnP.Text = "Submit";
+            this.btnP.UseVisualStyleBackColor = true;
+            this.btnP.Visible = false;
+            this.btnP.Click += new System.EventHandler(this.btnP_Click);
+            // 
+            // txtP3
+            // 
+            this.txtP3.Location = new System.Drawing.Point(613, 57);
+            this.txtP3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtP3.Name = "txtP3";
+            this.txtP3.Size = new System.Drawing.Size(114, 27);
+            this.txtP3.TabIndex = 5;
+            this.txtP3.Visible = false;
+            // 
+            // txtP2
+            // 
+            this.txtP2.Location = new System.Drawing.Point(310, 57);
+            this.txtP2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtP2.Name = "txtP2";
+            this.txtP2.Size = new System.Drawing.Size(114, 27);
+            this.txtP2.TabIndex = 4;
+            this.txtP2.Visible = false;
+            // 
+            // lblP3
+            // 
+            this.lblP3.AutoSize = true;
+            this.lblP3.Location = new System.Drawing.Point(613, 20);
+            this.lblP3.Name = "lblP3";
+            this.lblP3.Size = new System.Drawing.Size(25, 20);
+            this.lblP3.TabIndex = 3;
+            this.lblP3.Text = "P3";
+            this.lblP3.Visible = false;
+            // 
+            // lblP2
+            // 
+            this.lblP2.AutoSize = true;
+            this.lblP2.Location = new System.Drawing.Point(310, 20);
+            this.lblP2.Name = "lblP2";
+            this.lblP2.Size = new System.Drawing.Size(25, 20);
+            this.lblP2.TabIndex = 2;
+            this.lblP2.Text = "P2";
+            this.lblP2.Visible = false;
+            // 
+            // txtP1
+            // 
+            this.txtP1.Location = new System.Drawing.Point(24, 57);
+            this.txtP1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtP1.Name = "txtP1";
+            this.txtP1.Size = new System.Drawing.Size(114, 27);
+            this.txtP1.TabIndex = 1;
+            this.txtP1.Visible = false;
+            // 
+            // lblP1
+            // 
+            this.lblP1.AutoSize = true;
+            this.lblP1.Location = new System.Drawing.Point(24, 20);
+            this.lblP1.Name = "lblP1";
+            this.lblP1.Size = new System.Drawing.Size(25, 20);
+            this.lblP1.TabIndex = 0;
+            this.lblP1.Text = "P1";
+            this.lblP1.Visible = false;
+            // 
+            // lblCV
+            // 
+            this.lblCV.AutoSize = true;
+            this.lblCV.Location = new System.Drawing.Point(115, 20);
+            this.lblCV.Name = "lblCV";
+            this.lblCV.Size = new System.Drawing.Size(12, 20);
+            this.lblCV.TabIndex = 4;
+            this.lblCV.Text = ".";
+            // 
+            // btnC1
+            // 
+            this.btnC1.Location = new System.Drawing.Point(29, 57);
+            this.btnC1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC1.Name = "btnC1";
+            this.btnC1.Size = new System.Drawing.Size(100, 50);
+            this.btnC1.TabIndex = 5;
+            this.btnC1.Text = "Consulta A";
+            this.btnC1.UseVisualStyleBackColor = true;
+            this.btnC1.Click += new System.EventHandler(this.btnC1_Click);
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(270, 57);
+            this.btnC2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(100, 50);
+            this.btnC2.TabIndex = 6;
+            this.btnC2.Text = "Consulta B";
+            this.btnC2.UseVisualStyleBackColor = true;
+            this.btnC2.Click += new System.EventHandler(this.btnC2_Click);
+            // 
+            // btnC3
+            // 
+            this.btnC3.Location = new System.Drawing.Point(506, 57);
+            this.btnC3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC3.Name = "btnC3";
+            this.btnC3.Size = new System.Drawing.Size(100, 50);
+            this.btnC3.TabIndex = 7;
+            this.btnC3.Text = "Consulta C";
+            this.btnC3.UseVisualStyleBackColor = true;
+            this.btnC3.Click += new System.EventHandler(this.btnC3_Click);
+            // 
+            // btnC4
+            // 
+            this.btnC4.Location = new System.Drawing.Point(713, 61);
+            this.btnC4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC4.Name = "btnC4";
+            this.btnC4.Size = new System.Drawing.Size(100, 50);
+            this.btnC4.TabIndex = 8;
+            this.btnC4.Text = "Consulta D";
+            this.btnC4.UseVisualStyleBackColor = true;
+            this.btnC4.Click += new System.EventHandler(this.btnC4_Click);
+            // 
+            // btnC5
+            // 
+            this.btnC5.Location = new System.Drawing.Point(942, 65);
+            this.btnC5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC5.Name = "btnC5";
+            this.btnC5.Size = new System.Drawing.Size(100, 50);
+            this.btnC5.TabIndex = 9;
+            this.btnC5.Text = "Consulta E";
+            this.btnC5.UseVisualStyleBackColor = true;
+            this.btnC5.Click += new System.EventHandler(this.btnC5_Click);
+            // 
+            // btnC6
+            // 
+            this.btnC6.Location = new System.Drawing.Point(27, 156);
+            this.btnC6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC6.Name = "btnC6";
+            this.btnC6.Size = new System.Drawing.Size(100, 50);
+            this.btnC6.TabIndex = 10;
+            this.btnC6.Text = "Consulta F";
+            this.btnC6.UseVisualStyleBackColor = true;
+            this.btnC6.Click += new System.EventHandler(this.btnC6_Click);
+            // 
+            // btnC7
+            // 
+            this.btnC7.Location = new System.Drawing.Point(270, 156);
+            this.btnC7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC7.Name = "btnC7";
+            this.btnC7.Size = new System.Drawing.Size(100, 50);
+            this.btnC7.TabIndex = 11;
+            this.btnC7.Text = "Consulta G";
+            this.btnC7.UseVisualStyleBackColor = true;
+            this.btnC7.Click += new System.EventHandler(this.btnC7_Click);
+            // 
+            // btnC8
+            // 
+            this.btnC8.Location = new System.Drawing.Point(506, 155);
+            this.btnC8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC8.Name = "btnC8";
+            this.btnC8.Size = new System.Drawing.Size(100, 50);
+            this.btnC8.TabIndex = 12;
+            this.btnC8.Text = "Consulta H";
+            this.btnC8.UseVisualStyleBackColor = true;
+            this.btnC8.Click += new System.EventHandler(this.btnC8_Click);
+            // 
+            // btnC9
+            // 
+            this.btnC9.Location = new System.Drawing.Point(712, 156);
+            this.btnC9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC9.Name = "btnC9";
+            this.btnC9.Size = new System.Drawing.Size(100, 50);
+            this.btnC9.TabIndex = 13;
+            this.btnC9.Text = "Consulta I";
+            this.btnC9.UseVisualStyleBackColor = true;
+            this.btnC9.Click += new System.EventHandler(this.btnC9_Click);
+            // 
+            // btnC10
+            // 
+            this.btnC10.Location = new System.Drawing.Point(942, 155);
+            this.btnC10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnC10.Name = "btnC10";
+            this.btnC10.Size = new System.Drawing.Size(100, 50);
+            this.btnC10.TabIndex = 14;
+            this.btnC10.Text = "Consulta J";
+            this.btnC10.UseVisualStyleBackColor = true;
+            this.btnC10.Click += new System.EventHandler(this.btnC10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 667);
+            this.Controls.Add(this.btnC10);
+            this.Controls.Add(this.btnC9);
+            this.Controls.Add(this.btnC8);
+            this.Controls.Add(this.btnC7);
+            this.Controls.Add(this.btnC6);
+            this.Controls.Add(this.btnC5);
+            this.Controls.Add(this.btnC4);
+            this.Controls.Add(this.btnC3);
+            this.Controls.Add(this.btnC2);
+            this.Controls.Add(this.btnC1);
+            this.Controls.Add(this.lblCV);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblConsultas);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label lblConsultas;
+        private Panel panel1;
+        private Button btnP;
+        private TextBox txtP3;
+        private TextBox txtP2;
+        private Label lblP3;
+        private Label lblP2;
+        private TextBox txtP1;
+        private Label lblP1;
+        private Label lblCV;
+        private Button btnC1;
+        private Button btnC2;
+        private Button btnC3;
+        private Button btnC4;
+        private Button btnC5;
+        private Button btnC6;
+        private Button btnC7;
+        private Button btnC8;
+        private Button btnC9;
+        private Button btnC10;
+        private DateTimePicker dtp2;
+        private DateTimePicker dtp1;
+    }
+}
